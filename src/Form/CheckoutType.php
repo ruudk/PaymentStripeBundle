7ac40e60ac12d34224b82f9869f7f7bd6168f8f1
tdubuffet <dubuffet.thibault@gmail.com>
2017-04-18 14:53:46 +0200

    Fix type form SF3

--- src/Form/CheckoutType.php
+++ src/Form/CheckoutType.php
@@ -8,7 +8,7 @@ class CheckoutType extends StripeType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('token', 'hidden', array(
+        $builder->add('token', HiddenType::class, array(
             'required' => false
         ));
     }
